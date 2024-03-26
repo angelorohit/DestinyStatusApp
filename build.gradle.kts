75725ed09b8f2b93b732637b2a2738cc94a9402e
angelorohit <angelorohit@gmail.com>
2024-03-26 08:18:49 +0800

    Add pre-commit script that runs static analysis tool.
    Signed-off-by: angelorohit <angelorohit@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,3 +3,18 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.detekt) apply false
 }
+
+task("addPreCommitGitHookOnBuild") {
+    println("⚈ ⚈ ⚈ Running Add Pre-Commit Git Hook Script on Build ⚈ ⚈ ⚈")
+    // Copy the pre-commit hook
+    copy {
+        from("./.scripts/pre-commit")
+        into("./.git/hooks")
+    }
+
+    // Make the pre-commit hook executable
+    val hookFile = file("./.git/hooks/pre-commit")
+    hookFile.setExecutable(true)
+
+    println("✅ Added Pre Commit Git Hook Script.")
+}
