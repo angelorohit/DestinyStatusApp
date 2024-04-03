945be24aea3f71f8d9030dded690d83da9ca1a88
angelorohit <angelorohit@gmail.com>
2024-04-03 19:25:14 +0800

    Add EXTRA_REFERRER for CustomTabsLauncher.
    Signed-off-by: angelorohit <angelorohit@gmail.com>

--- app/src/main/java/com/angelo/destinystatusapp/data/remote/RemoteDataSourceImpl.kt
+++ app/src/main/java/com/angelo/destinystatusapp/data/remote/RemoteDataSourceImpl.kt
@@ -5,11 +5,15 @@ import com.angelo.destinystatusapp.data.remote.exception.RequestErrorException
 import com.angelo.destinystatusapp.data.remote.model.BungieHelpUpdatesResponse
 
 class RemoteDataSourceImpl(private val destinyStatusService: DestinyStatusService) : RemoteDataSource {
+    companion object {
+        private const val MAX_UPDATE_ITEMS = 20
+    }
+
     override suspend fun fetchBungieHelpUpdates(): List<BungieHelpUpdatesResponse> {
         val response = destinyStatusService.fetchBungieHelpUpdates()
         with(response) {
             if (isSuccessful) {
-                return body() ?: throw RequestErrorException(code(), "Empty response body")
+                return body()?.take(MAX_UPDATE_ITEMS) ?: throw RequestErrorException(code(), "Empty response body")
             } else {
                 throw RequestErrorException(code(), errorBody()?.string() ?: "")
             }
--- app/src/main/java/com/angelo/destinystatusapp/presentation/helper/customtabs/CustomTabsLauncher.kt
+++ app/src/main/java/com/angelo/destinystatusapp/presentation/helper/customtabs/CustomTabsLauncher.kt
@@ -1,12 +1,16 @@
 package com.angelo.destinystatusapp.presentation.helper.customtabs
 
 import android.content.Context
+import android.content.Intent
 import android.net.Uri
 import androidx.browser.customtabs.CustomTabsIntent
 
 fun Context.launchCustomTabs(url: String) {
-    CustomTabsIntent.Builder()
+    val customTabsIntent = CustomTabsIntent.Builder()
         .setShareState(CustomTabsIntent.SHARE_STATE_ON)
         .setShowTitle(true)
-        .build().launchUrl(this, Uri.parse(url))
+        .build()
+
+    customTabsIntent.intent.putExtra(Intent.EXTRA_REFERRER, Uri.parse("android-app://$packageName"))
+    customTabsIntent.launchUrl(this, Uri.parse(url))
 }
