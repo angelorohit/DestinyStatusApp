d2121e0c86fcfdc410f9a15b89dbeb7e47bed4d2
angelorohit <angelorohit@gmail.com>
2024-04-13 00:14:12 +0800

    Add compile and target SDK previews for Android 15.
    Signed-off-by: angelorohit <angelorohit@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,11 +11,14 @@ plugins {
 android {
     namespace = "com.angelo.destinystatusapp"
     compileSdk = 34
+    compileSdkPreview = "VanillaIceCream"
 
     defaultConfig {
         applicationId = "com.angelo.destinystatusapp"
         minSdk = 24
         targetSdk = 34
+        targetSdkPreview = "VanillaIceCream"
+
         versionCode = 1
         versionName = "1.0.0"
 
