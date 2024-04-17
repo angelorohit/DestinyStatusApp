980a3d7782f1e896d26d83f48f78d986084d0a4a
angelorohit <angelorohit@gmail.com>
2024-04-17 08:06:02 +0800

    Remove aspect ratio calculation since it doesn't have any effect.
    Signed-off-by: angelorohit <angelorohit@gmail.com>

--- app/src/main/java/com/angelo/destinystatusapp/presentation/widget/ZoomableImage.kt
+++ app/src/main/java/com/angelo/destinystatusapp/presentation/widget/ZoomableImage.kt
@@ -1,6 +1,5 @@
 package com.angelo.destinystatusapp.presentation.widget
 
-import android.content.res.Configuration
 import androidx.compose.foundation.gestures.rememberTransformableState
 import androidx.compose.foundation.gestures.transformable
 import androidx.compose.foundation.layout.BoxWithConstraints
@@ -16,7 +15,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 import coil.compose.SubcomposeAsyncImage
 import coil.request.ImageRequest
@@ -25,13 +23,9 @@ import coil.request.ImageRequest
 fun ZoomableImage(photoUrl: String, photoAspectRatio: Float, modifier: Modifier = Modifier) {
     var scale by remember { mutableFloatStateOf(1f) }
     var offset by remember { mutableStateOf(Offset.Zero) }
-    val aspectRatio = minOf(
-        photoAspectRatio,
-        calculateScreenAspectRatio(LocalConfiguration.current),
-    )
 
     BoxWithConstraints(
-        modifier = modifier.aspectRatio(aspectRatio),
+        modifier = modifier.aspectRatio(photoAspectRatio),
     ) {
         val state = rememberTransformableState { zoomChange, panChange, _ ->
             scale = (scale * zoomChange).coerceIn(1f, 2f)
@@ -69,16 +63,5 @@ fun ZoomableImage(photoUrl: String, photoAspectRatio: Float, modifier: Modifier 
             },
             contentDescription = null,
         )
-    }
-}
-
-private fun calculateScreenAspectRatio(configuration: Configuration): Float {
-    val screenWidthDp = configuration.screenWidthDp
-    val screenHeightDp = configuration.screenHeightDp
-
-    return if (configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) {
-        screenWidthDp.toFloat() / screenHeightDp
-    } else {
-        screenHeightDp.toFloat() / screenWidthDp
     }
 }
