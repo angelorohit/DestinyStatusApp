a3474de732bbc52321cbc4290857c9f69f3cf69f
angelorohit <angelorohit@gmail.com>
2024-04-20 19:03:26 +0800

    Improve settings animation.
    Signed-off-by: angelorohit <angelorohit@gmail.com>

--- app/src/main/java/com/angelo/destinystatusapp/presentation/screen/SettingsScreen.kt
+++ app/src/main/java/com/angelo/destinystatusapp/presentation/screen/SettingsScreen.kt
@@ -4,9 +4,9 @@ import android.content.Intent
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
@@ -16,10 +16,12 @@ import androidx.compose.material3.ListItem
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
@@ -44,7 +46,19 @@ fun SettingsScreen(navController: NavController, modifier: Modifier = Modifier) 
     Scaffold(
         modifier = modifier,
         topBar = {
-            StandardTopAppBar(navController = navController)
+            LottieAnimation(
+                composition = composition,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(150.dp),
+                contentScale = ContentScale.Fit,
+            )
+            StandardTopAppBar(
+                navController = navController,
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = Color.Transparent,
+                ),
+            )
         },
         content = { innerPadding ->
             Column(
@@ -53,13 +67,6 @@ fun SettingsScreen(navController: NavController, modifier: Modifier = Modifier) 
                     .verticalScroll(rememberScrollState()),
                 horizontalAlignment = Alignment.CenterHorizontally,
             ) {
-                LottieAnimation(
-                    composition = composition,
-                    modifier = Modifier
-                        .size(250.dp),
-                    contentScale = ContentScale.Fit,
-                )
-
                 Card(modifier = Modifier.padding(16.dp)) {
                     Text(
                         text = stringResource(R.string.community_driven),
