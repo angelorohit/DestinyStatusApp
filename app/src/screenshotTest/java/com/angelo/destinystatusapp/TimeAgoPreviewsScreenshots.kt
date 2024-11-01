027715728162b6a5eeaafbcff9ced1cb42dc7ba5
angelorohit <angelorohit@gmail.com>
2024-11-01 22:25:05 +0800

    Add test resources to screenshotTest folder.
    Signed-off-by: angelorohit <angelorohit@gmail.com>

--- app/src/screenshotTest/java/com/angelo/destinystatusapp/TimeAgoPreviewsScreenshots.kt
+++ app/src/screenshotTest/java/com/angelo/destinystatusapp/TimeAgoPreviewsScreenshots.kt
@@ -9,7 +9,7 @@ import com.angelo.destinystatusapp.domain.helper.datetime.clock.testing.FakeCloc
 import com.angelo.destinystatusapp.domain.model.BungiePost
 import com.angelo.destinystatusapp.presentation.theme.DestinyStatusAppTheme
 import com.angelo.destinystatusapp.presentation.widget.TimeAgoText
-import kotlin.time.Duration.Companion.milliseconds
+import kotlin.time.Duration.Companion.seconds
 
 @Stable
 @Suppress("unused")
@@ -17,8 +17,56 @@ class TimeAgoPreviewsScreenshots {
 
     @PreviewLightDark
     @Composable
+    fun TimeAgoTextMomentsAgoPreview(modifier: Modifier = Modifier) {
+        val fakeClock = FakeClock(1730455131.seconds)
+        DestinyStatusAppTheme {
+            Surface {
+                TimeAgoText(
+                    bungiePost = BungiePost(
+                        id = "0",
+                        createdAt = "March 21, 2024, 21:55:02 UTC",
+                        userName = "BungieHelp",
+                        text = "The quick brown fox jumps over the lazy dog\n\nThis is a test\n7.3.5.2\nhttps://t.co/",
+                        timestamp = 1730455131.seconds,
+                        url = "https://twitter.com/BungieHelp/status/1770932153981050919",
+                        media = emptyList(),
+                        isRepost = false,
+                    ),
+                    clock = fakeClock,
+                    modifier = modifier,
+                )
+            }
+        }
+    }
+
+    @PreviewLightDark
+    @Composable
+    fun TimeAgoTextMinutesAgoPreview(modifier: Modifier = Modifier) {
+        val fakeClock = FakeClock((1730455131 + 120).seconds)
+        DestinyStatusAppTheme {
+            Surface {
+                TimeAgoText(
+                    bungiePost = BungiePost(
+                        id = "0",
+                        createdAt = "March 21, 2024, 21:55:02 UTC",
+                        userName = "BungieHelp",
+                        text = "The quick brown fox jumps over the lazy dog\n\nThis is a test\n7.3.5.2\nhttps://t.co/",
+                        timestamp = 1730455131.seconds,
+                        url = "https://twitter.com/BungieHelp/status/1770932153981050919",
+                        media = emptyList(),
+                        isRepost = false,
+                    ),
+                    clock = fakeClock,
+                    modifier = modifier,
+                )
+            }
+        }
+    }
+
+    @PreviewLightDark
+    @Composable
     fun TimeAgoTextMissingTimestampPreview(modifier: Modifier = Modifier) {
-        val fakeClock = FakeClock(1711999999.milliseconds)
+        val fakeClock = FakeClock(1730455131.seconds)
         DestinyStatusAppTheme {
             Surface {
                 TimeAgoText(
