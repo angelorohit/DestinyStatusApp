334cacffb2938b7f8cbfe3371bbaca865abcf831
angelorohit <angelorohit@gmail.com>
2025-06-16 21:14:01 +0800

    Fix post link colors.
    Signed-off-by: angelorohit <angelorohit@gmail.com>

--- app/src/main/java/com/angelo/destinystatusapp/presentation/widget/BungiePostCard.kt
+++ app/src/main/java/com/angelo/destinystatusapp/presentation/widget/BungiePostCard.kt
@@ -74,7 +74,7 @@ fun BungiePostCard(
             LinkifyText(
                 modifier = Modifier.padding(top = 8.dp, start = 16.dp, end = 16.dp, bottom = 16.dp),
                 text = bungiePost.text.orEmpty(),
-                linkColor = MaterialTheme.colorScheme.onPrimaryContainer,
+                linkColor = MaterialTheme.colorScheme.onSurface,
                 style = MaterialTheme.typography.bodyLarge,
             )
 
